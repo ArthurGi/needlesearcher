d324f8b5409b8e0c92ab2c2b54d29aa38a18cd11
Artur Gilmanov <artur.gilmanov.93@mail.ru>
2020-03-30 18:53:40 +0300

    init repo

--- example.php
+++ example.php
@@ -2,6 +2,7 @@
 /**
  *    $convertedText = mb_convert_encoding($text, 'utf-8', mb_detect_encoding($text));
  *
+ *    выкидывть экзпшны ??
  *    прокинуть кодировку после определения ??
  *    учесть случаи с регистром и без ??
  */
